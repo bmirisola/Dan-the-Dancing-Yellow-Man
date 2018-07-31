16e8932aafa799dab5ebeb657359e81f14f437f8
Benny Mirisola <bennymirisola@gmail.com>
2018-07-31 12:34:57 -0400

    Reformatted imports and code layout

--- app/src/main/java/zabbandco/danthedancingyellowman/MainActivity.java
+++ app/src/main/java/zabbandco/danthedancingyellowman/MainActivity.java
@@ -1,28 +1,8 @@
 package zabbandco.danthedancingyellowman;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.provider.ContactsContract;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.Button;
-import android.content.Intent;
-import android.telephony.SmsManager;
-import android.widget.TextView;
-import android.widget.Toast;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 
 // <string name="banner_ad_unit_id">ca-app-pub-3940256099942544/6300978111</string> - test
 // <string name="banner_ad_unit_id">ca-app-pub-9744674999881322/4453831094</string> - real bottom
@@ -47,15 +27,15 @@ public class MainActivity extends Activity {
         super.onResume();
     }
 
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
     }
 
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
     }
 
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
     }
 }
